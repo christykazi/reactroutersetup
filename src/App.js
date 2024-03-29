c43bddff758a9667d538f838014ec51edccf460f
christykazi <christyagbai4178@gmail.com>
2024-03-29 23:39:53 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
       <Route path="/about" element = {<About/>}/>
       <Route path="/services" element ={<Services/>}/>
       <Route path='/users' element ={<Users/>}/>
+    
     </Routes>
    </BrowserRouter>
   )
